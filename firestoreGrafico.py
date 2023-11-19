8a6aeb91aec823f98b6290c57e63cf6816545024
informaticosonline <informaticosonline22@gmail.com>
2023-11-19 01:29:59 +0100

    Initial commit

---
+++ firestoreGrafico.py
@@ -0,0 +1,90 @@
+import tkinter as tk
+from tkinter import messagebox
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# Configuración inicial de Firebase
+cred = credentials.Certificate(r'C:/Users/clja1/Downloads/alpine-charge-403816-508bb90d3f34.json')
+firebase_admin.initialize_app(cred)
+
+# Inicializar la instancia de Firestore
+db = firestore.client()
+
+# Funciones de Firebase
+def add_data(collection_id, document_id, data):
+    db.collection(collection_id).document(document_id).set(data)
+
+def get_all_data(collection_id):
+    documents = db.collection(collection_id).stream()
+    data = []
+    for doc in documents:
+        doc_data = doc.to_dict()
+        doc_data['id'] = doc.id
+        data.append(doc_data)
+    return data
+
+def delete_data(collection_id, document_id):
+    db.collection(collection_id).document(document_id).delete()
+
+# Funciones de la interfaz Tkinter
+def actualizar_datos():
+    # Limpia la lista actual
+    lista_alumnos.delete(0, tk.END)
+
+    # Obtiene y muestra los datos actualizados
+    todos_los_alumnos = get_all_data('alumnos')
+    for alumno in todos_los_alumnos:
+        # Asegúrate de que cada alumno tenga un ID y un nombre
+        # Utiliza .get() para manejar casos donde 'nombre' podría no estar presente
+        id_alumno = alumno.get('id', 'ID Desconocido')
+        nombre_alumno = alumno.get('nombre', 'Nombre Desconocido')
+
+        # Inserta la información del alumno en la lista
+        lista_alumnos.insert(tk.END, f"{id_alumno}: {nombre_alumno}")
+
+
+def agregar_alumno():
+    nuevo_id = entrada_id.get()
+    nuevo_nombre = entrada_nombre.get()
+    add_data('alumnos', nuevo_id, {'nombre': nuevo_nombre})
+    actualizar_datos()
+
+def eliminar_alumno():
+    seleccion = lista_alumnos.curselection()
+    if seleccion:
+        id_seleccionado = lista_alumnos.get(seleccion[0]).split(':')[0]
+        delete_data('alumnos', id_seleccionado)
+        actualizar_datos()
+
+# Crear la ventana principal
+raiz = tk.Tk()
+raiz.title("Gestión de Alumnos")
+
+# Crear widgets
+etiqueta_id = tk.Label(raiz, text="ID del Alumno:")
+etiqueta_id.pack()
+
+entrada_id = tk.Entry(raiz)
+entrada_id.pack()
+
+etiqueta_nombre = tk.Label(raiz, text="Nombre del Alumno:")
+etiqueta_nombre.pack()
+
+entrada_nombre = tk.Entry(raiz)
+entrada_nombre.pack()
+
+boton_agregar = tk.Button(raiz, text="Agregar Alumno", command=agregar_alumno)
+boton_agregar.pack()
+
+boton_eliminar = tk.Button(raiz, text="Eliminar Alumno Seleccionado", command=eliminar_alumno)
+boton_eliminar.pack()
+
+lista_alumnos = tk.Listbox(raiz)
+lista_alumnos.pack()
+
+# Inicializar la lista con los datos actuales
+actualizar_datos()
+
+# Ejecutar el bucle principal
+raiz.mainloop()
+
---
+++ firestore_example.py
@@ -0,0 +1,72 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Usar el archivo json descargado
+cred = credentials.Certificate(r'C:/Users/clja1/Downloads/alpine-charge-403816-508bb90d3f34.json')
+firebase_admin.initialize_app(cred)
+
+# Inicializa la instancia de Firestore
+db = firestore.client()
+
+# Función para agregar datos en el caso que no se quiera generar la id automáticamente
+def add_data(collection_id, document_id, data):
+    db.collection(collection_id).document(document_id).set(data)
+
+#Función para agregar un documento con ID generado automáticamente
+def add_data_auto_id(collection_id, data):
+    doc_ref = db.collection(collection_id).add(data)
+    return doc_ref
+
+#Funcion para obtener todos los datos de una colección
+def get_all_data(collection_id):
+    documents = db.collection(collection_id).stream()
+    data = []
+    for doc in documents:
+        doc_data = doc.to_dict()
+        # Opcional, si quieres incluir el ID del documento
+        doc_data['id'] = doc.id  
+        data.append(doc_data)
+    return data
+
+# Función para obtener datos concretos
+def get_data(collection_id, document_id):
+    doc_ref = db.collection(collection_id).document(document_id)
+    doc = doc_ref.get()
+    if doc.exists:
+        return doc.to_dict()
+    else:
+        return None
+
+# Ejemplo de uso para agregar datos
+'''
+data = {
+    'name': 'Martin Marin',
+    'age': 20,
+    'email': 'martinmarin@example.com'
+}
+
+add_data_auto_id('profesor', data)
+'''
+
+
+
+# Obtener datos
+
+# Todos los alumnos
+todos_alumnos = get_all_data('alumnos')
+#Todos los users
+todos_users = get_all_data('users')
+
+for alumno in todos_alumnos:
+    print(alumno)
+
+for user in todos_users:
+    print(user)
+
+#especificos
+
+user_data = get_data('users', 'user_2')
+alumnos_data = get_data('alumnos','1UcwfQA0QPZhbsPnRnNA')
+
+print(user_data,alumnos_data)
